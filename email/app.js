c946b237df175d7b92cf046e5852d5b5f0e20598
Mithu Kumar Gupta <73178363+mithu2001@users.noreply.github.com>
2023-07-31 14:07:15 +0530

    Update app.js

--- email/app.js
+++ email/app.js
@@ -3,7 +3,7 @@ const mailer = require('nodemailer');
 let transport = mailer.createTransport({
     service:'gmail',
     auth:{
-        user:'ifallinlove14369@gmail.com',
+        user:'userEmail@gmail.com',
         pass:'App_password'
     }
 })
@@ -12,19 +12,11 @@ let transport = mailer.createTransport({
 
 
 let messageObj = {
-    From:'ifallinlove14369@gmail.com',
-    to:'kumarmithu2021@gmail.com',
+    From:'user@gmail.com',
+    to:'recieveremail@gmail.com',
     subject:'Dil ❤❤ ka Matter hai',
     text:` 
-    इस इश्क़ ने हमे मगरूर कर दिया ,
-    हर खुशी ☺️ से बहुत दूर कर दिया …
-    सोचा नहीं था कभी हमे इश्क़ ♥️ होगा ,
-    पर आपके नजरों ने मजबूर कर दिया … ।।
-    I promise to love you a little bit more every day. 
-    I want you in my life now and forever. 
-    I want to hold onto your hands and keep you safe in my arms forever. 
-    I love you.
-         - Unknown Lover
+       write your message 
     `
 }
 
